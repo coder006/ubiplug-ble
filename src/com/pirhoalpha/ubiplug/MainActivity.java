071d2776e85fc35bfa5c8ef6dc59a3cd71d02ec8
Sudhanshu Mishra <mrsud94@gmail.com>
2013-12-22 03:19:22 +0530

    Completed v1

--- src/com/pirhoalpha/ubiplug/MainActivity.java
+++ src/com/pirhoalpha/ubiplug/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pirhoalpha.ubiplug;
 
+import java.util.ArrayList;
 import java.util.UUID;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
@@ -21,7 +22,10 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Message;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.os.Bundle;
@@ -31,12 +35,15 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements BluetoothAdapter.LeScanCallback{
 	
 	private boolean mScanning;	//Boolean to store state of scanning
-	private TextView noOfDevices;	//TextView to show no of devices found
+	private TextView messageView;	//TextView to show no of devices found
+    private ListView deviceListView;
 	private BluetoothAdapter mBluetoothAdapter;
 	private BluetoothGatt mConnectedGatt;
 	private SparseArray<BluetoothDevice> mDevices;
 	private Handler mHandler;
 	private ProgressBar mProgress;	//ProgressBar to show scanning progress
+    public static ArrayList<BluetoothDevice> myBTDevices = new ArrayList<BluetoothDevice>();
+    private ArrayAdapter<String> mArrayAdapter;
 	private static final String TAG = "BluetoothGattActivity";
 	private final static int REQUEST_ENABLE_BT = 1;
 	private static final String DEVICE_NAME = "Device Name";	
@@ -59,9 +66,10 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_main);
-		noOfDevices = (TextView)findViewById(R.id.noOfDevices);
-		noOfDevices.setText("");
+		messageView = (TextView)findViewById(R.id.message_view);
+		messageView.setText("");
 		mProgress = (ProgressBar)findViewById(R.id.progressBar);
+		deviceListView = (ListView)findViewById(R.id.listView);
 		BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
 		mBluetoothAdapter = manager.getAdapter();
 	}
@@ -102,6 +110,7 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
 		    finish();
 		    return;
 		}
+        mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
 	}
 	
 	/**
@@ -171,13 +180,34 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
         return true;
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_scan:
+                mDevices.clear();
+                startScan();
+                return true;
+            default:
+                //Obtain the discovered device to connect with
+                BluetoothDevice device = mDevices.get(item.getItemId());
+                Log.i(TAG, "Connecting to "+device.getName());
+                /*
+                 * Make a connection with the device using the special LE-specific
+                 * connectGatt() method, passing in a callback for GATT events
+                 */
+                mConnectedGatt = device.connectGatt(this, true, mGattCallback);
+                //Display progress UI
+                mHandler.sendMessage(Message.obtain(null, MSG_PROGRESS, "Connecting to "+device.getName()+"..."));
+                return super.onOptionsItemSelected(item);
+        }
+    }
     /**
      * Function which starts scanning
      */
     private void startScan() 
     {
         mBluetoothAdapter.startLeScan(this);
-        
+        mProgress.setVisibility(1);
         mHandler.postDelayed(mStopRunnable, 2500);
     }
 
@@ -187,7 +217,7 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
     private void stopScan() 
     {
         mBluetoothAdapter.stopLeScan(this);
-        setProgressBarIndeterminateVisibility(false);
+        mProgress.setVisibility(0);
     }
 
 
@@ -200,8 +230,143 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
         Log.i(TAG, "New LE Device: " + device.getName() + " @ " + rssi);
         if (DEVICE_NAME.equals(device.getName())) 
         {
+        	mArrayAdapter.add(device.getName() + "  " + device.getAddress());
+        	mArrayAdapter.notifyDataSetChanged();
             mDevices.put(device.hashCode(), device);
-            invalidateOptionsMenu();
+            
         }
     }
+    
+    private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+
+        /* State Machine Tracking */
+        private int mState = 0;
+
+        private void reset() { mState = 0; }
+
+        private void advance() { mState++; }
+
+        /*
+         * Send an enable command to each sensor by writing a configuration
+         * characteristic.  This is specific to the SensorTag to keep power
+         * low by disabling sensors you aren't using.
+         */
+        private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+
+                         
+
+            @Override
+            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                Log.d(TAG, "Connection State Change: "+status+" -> "+connectionState(newState));
+                if (status == BluetoothGatt.GATT_SUCCESS && newState == BluetoothProfile.STATE_CONNECTED) {
+                    /*
+                     * Once successfully connected, we must next discover all the services on the
+                     * device before we can read and write their characteristics.
+                     */
+                    gatt.discoverServices();
+                    mHandler.sendMessage(Message.obtain(null, MSG_PROGRESS, "Discovering Services..."));
+                } else if (status == BluetoothGatt.GATT_SUCCESS && newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    /*
+                     * If at any point we disconnect, send a message to clear the weather values
+                     * out of the UI
+                     */
+                    mHandler.sendEmptyMessage(MSG_CLEAR);
+                } else if (status != BluetoothGatt.GATT_SUCCESS) {
+                    /*
+                     * If there is a failure at any stage, simply disconnect
+                     */
+                    gatt.disconnect();
+                }
+            }
+
+            @Override
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                Log.d(TAG, "Services Discovered: "+status);
+                /*
+                 * With services discovered, we are going to reset our state machine and start
+                 * working through the sensors we need to enable
+                 */
+            }
+
+            @Override
+            public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                //For each read, pass the data up to the UI thread to update the display
+                if (UBIPLUG_DATA_CHAR.equals(characteristic.getUuid())) {
+                    mainHandler.sendMessage(Message.obtain(null, MSG_CURRENT, characteristic));
+                }
+
+                //After reading the initial value, next we enable notifications
+            }
+
+            @Override
+            public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                /*
+                 * After notifications are enabled, all updates from the device on characteristic
+                 * value changes will be posted here.  Similar to read, we hand these up to the
+                 * UI thread to update the display.
+                 */
+                if (UBIPLUG_DATA_CHAR.equals(characteristic.getUuid())) {
+                    mainHandler.sendMessage(Message.obtain(null, MSG_CURRENT, characteristic));
+                }
+            }
+
+            @Override
+            public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+                //Once notifications are enabled, we move to the next sensor and start over with enable
+                advance();
+            }
+
+            @Override
+            public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+                Log.d(TAG, "Remote RSSI: "+rssi);
+            }
+
+            private String connectionState(int status) {
+                switch (status) {
+                    case BluetoothProfile.STATE_CONNECTED:
+                        return "Connected";
+                    case BluetoothProfile.STATE_DISCONNECTED:
+                        return "Disconnected";
+                    case BluetoothProfile.STATE_CONNECTING:
+                        return "Connecting";
+                    case BluetoothProfile.STATE_DISCONNECTING:
+                        return "Disconnecting";
+                    default:
+                        return String.valueOf(status);
+                }
+            }
+        };
+    };
+    
+    private static final int MSG_CURRENT = 103;
+    private static final int MSG_PROGRESS = 201;
+    private static final int MSG_DISMISS = 202;
+    private static final int MSG_CLEAR = 301;
+    private Handler mainHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            BluetoothGattCharacteristic characteristic;
+            switch (msg.what) {
+                case MSG_CURRENT:
+                    characteristic = (BluetoothGattCharacteristic) msg.obj;
+                    if (characteristic.getValue() == null) {
+                        Log.w(TAG, "Error obtaining cal value");
+                        return;
+                    }
+                    messageView.setText(characteristic.getStringValue(1));
+                    break;
+                case MSG_PROGRESS:
+                    messageView.setText((String) msg.obj);
+                    break;
+                case MSG_DISMISS:
+                    break;
+                case MSG_CLEAR:
+                	messageView.setText("");
+                    break;
+            }
+        }
+    };
+    
+    
+
 }
